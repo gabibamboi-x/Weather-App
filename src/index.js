173f613afd93ac185d8be454a9abfa9187d376d5
Gabriel Bamboi <gabi.bamboi_x@icloud.com>
2022-11-25 19:24:02 +0100

    Add the high & low temperatures

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import "./style/main.css"
+import "./style/main.css";
+import forecast from "./Images/forecast.svg"
 
-const input = document.querySelector('input')
+const input = document.querySelector('input');
+const cityName = document.querySelector('h1');
+const temperature = document.querySelector('.temp');
+const high = document.querySelector('.high');
+const low = document.querySelector('.low');
+const description = document.querySelector('.description');
+const forecastImg = document.querySelector('.forecastImg');
+
+const day1Forecast = document.querySelector('.day1');
+const day2Forecast = document.querySelector('.day2');
+const day3Forecast = document.querySelector('.day3');
+const day4Forecast = document.querySelector('.day4');
+const day5Forecast = document.querySelector('.day5');
+const day6Forecast = document.querySelector('.day6');
+const day7Forecast = document.querySelector('.day7');
+const day8Forecast = document.querySelector('.day8');
+const day9Forecast = document.querySelector('.day9');
+const day10Forecast = document.querySelector('.day10');
+
 let searchTerm = 'los angeles';
-let temperature = ' °F'
+let temp = ' °F';
+
+forecastImg.src = forecast
 
 async function getData() {
   try {
@@ -23,11 +44,15 @@ function getInfo() {
     if (response.cod === '404') { 
       input.value = ''
       response.message 
+      return
     }
 
-    document.querySelector('h1').innerText = response.name + ', ' + response.sys.country;
-    document.querySelector('.temp').innerText = 'Temperature: ' + response.main.temp + temperature;
-    document.querySelector('.humidity').innerText = 'Humidity: ' + response.main.humidity;
+    console.log(response)
+    cityName.innerText = response.name + ', ' + response.sys.country;
+    temperature.innerText = response.main.temp + temp;
+    description.innerText = response.weather[0].description
+    high.innerText = 'H: ' + response.main.temp_max + temp
+    low.innerText = 'L: ' + response.main.temp_min + temp
   })
   .catch(function(error) {
     console.log(error);
@@ -40,4 +65,5 @@ input.addEventListener('keypress', (event => {
   const key = event.key
   if (key === 'Enter') getInfo()
 }))
+
 
